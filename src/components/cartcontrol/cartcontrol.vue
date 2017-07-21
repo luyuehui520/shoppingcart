48d33b6bf7a669f3e59c17a245fda234cc0edb3b
sulu <sulu@suludeMacBook-Pro.local>
2017-07-21 15:04:52 +0800

    vue2.0project

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="cartcontrol">
+    <transition name="move">
+      <div class="cart-decrease " v-show="food.count>0" @click.stop.prevent="decreaseCart">
+        <i class="inner icon-remove_circle_outline"></i>
+      </div>
+    </transition>
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Vue from 'vue';
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    methods: {
+      addCart(event) {
+        // 阻止pc端本身的事件分发，使pc端与移动端保持一致
+        if (!event._constructed) {
+          return;
+        }
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count++;
+        }
+        this.$emit('add', event.target);
+      },
+      decreaseCart(event) {
+        if (!event._constructed) {
+          return;
+        }
+        if (this.food.count) {
+          this.food.count--;
+        }
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .cartcontrol
+    font-size: 0
+    .cart-decrease
+      display: inline-block
+      padding: 6px
+      opacity: 1
+      transition: all 0.4s linear
+      transform: translate3d(0, 0, 0)
+      .inner
+        display: inline-block
+        line-height: 24px
+        font-size: 24px
+        color: rgb(0, 160, 220)
+        transition: all 0.4s linear
+        transform: rotate(0)
+      &.move-enter, &.move-leave-to
+        opacity: 0
+        transform: translate3d(24px, 0, 0)
+        .inner
+          transform: rotate(180deg)
+    .cart-count
+      display: inline-block
+      vertical-align: top
+      width: 12px
+      line-height: 24px
+      padding-top: 6px
+      font-size: 10px
+      text-align: center
+      color: rgb(147, 153, 159)
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0, 160, 220)
+</style>
